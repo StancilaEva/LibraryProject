e515638a088cf2b401a55d1c5f67438602648a9e
Eva Stancila <eva.stancila@amdaris.com>
2022-03-14 13:19:45 +0200

    Added Address, Book, Client, Genre, Lend Classes

---
+++ Library/Library.Core/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Core
+{
+    public class Book
+    {
+        private string title;
+        private string author;
+        private Genre genre;
+
+        public Book()
+        {
+        }
+
+        public Book(string title, string author, Genre genre)
+        {
+            this.title = title;
+            this.author = author;
+            this.genre = genre;
+        }
+
+        public string Title { get { return title; } set { if(value.Length>=3)title = value; } }
+        public string Author { get { return author; } set { if(value.Length >= 3)author = value; } }
+        public Genre Genre { get { return genre; } set { genre = value; } }
+
+    }
+}
---
+++ Library/Library.Core/Lend.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Core
+{
+    public class Lend
+    {
+        private Book book;
+        private Client client;
+        private DateTime startDate;
+        private DateTime endDate;
+
+        public Lend()
+        {
+        }
+
+        public Lend(Book book, Client client, DateTime startDate, DateTime endDate)
+        {
+            this.book = book;
+            this.client = client;
+            this.startDate = startDate;
+            this.endDate = endDate;
+        }
+
+        public Book Book { get { return book; } set { book = value; } }
+       public Client Client { get { return client; } set { client = value; } }
+       public DateTime StartDate { get { return startDate; } set { if(DateOnly.FromDateTime(value)>=DateOnly.FromDateTime(DateTime.Today))startDate = value; } }
+       public DateTime EndDate { get { return endDate; } set { if(DateOnly.FromDateTime(value)>DateOnly.FromDateTime(DateTime.Today)) endDate = value; } }
+
+         
+    }
+}
