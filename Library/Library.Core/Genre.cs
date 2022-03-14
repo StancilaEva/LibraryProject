b76c3d6fe4b5668d5964c33aa5a5330c179cbc85
Eva Stancila <eva.stancila@amdaris.com>
2022-03-14 13:49:15 +0200

    Added Address, Book, Client, Genre, Lend Classes

---
+++ Library/Library.Core/Genre.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Core
+{
+    public enum Genre
+    {
+        HORROR,
+        COMEDY,
+        SCIFI,
+        FANTASY,
+        ROMANCE
+    }
+}
